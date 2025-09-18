3d1795c319af6b82006619bf5c60a169cd3b85f9
João Victor Reis <victorreislima186@gmail.com>
2025-09-18 15:21:46 -0300

    mudanca de pasta

--- backend/correios/settings.py
+++ backend/correios/settings.py
@@ -9,13 +9,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=v=oj-9j10rp!#9-n#q6rzfnh-6#pt%5q+^6%c0o#z=p0=i7f!'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = config("DEBUG", default=False, cast=bool)
+SECRET_KEY = config("SECRET_KEY")
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="*").split(",")
 
 
 # Application definition
